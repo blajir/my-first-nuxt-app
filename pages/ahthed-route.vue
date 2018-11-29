f7a315b51b29d1f570592c618487de02afa5a20e
junichi-hatano <junichi-hatano@hands-lab.com>
2018-11-29 10:47:47 +0900

    認証ページの追加

---
+++ pages/ahthed-route.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>認証が必要なページ</h2>
+    <p>
+      Cookieの'Credential'値がTruthyな場合のみアクセス可能です。<br>
+      <nuxt-link to="/">Topへ</nuxt-link>
+    </p>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>login</h2>
+    <p>
+      <button type="button" @click="login">login</button>
+      <nuxt-link to="/">Top</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+import Cookies from 'universal-cookie'
+
+export default {
+  methods: {
+    login() {
+      const cookies = new Cookies()
+      cookies.set('credential', 'true', {maxAge: 90})
+      this.$route.push('/')
+    }
+  }
+}
+</script>
